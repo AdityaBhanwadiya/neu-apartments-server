526eb9ee2d9725950f02a6a69fe3285d999d7420
aditya222001 <adityapatel020201@gmail.com>
2024-04-26 01:20:35 +0530

    commit

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
-import listingRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
-import cors from "cors";
 dotenv.config();
 
-mongoose  
-  .connect("mongodb+srv://arpanpatel1899:arpanpatel1899@cluster0.hawspx5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mongoose
+  .connect(process.env.MONGO)
   .then(() => {
     console.log('Connected to MongoDB!');
   })
@@ -21,14 +20,13 @@ mongoose
   const __dirname = path.resolve();
 
 const app = express();
-app.use(cors());
+
 app.use(express.json());
 
 app.use(cookieParser());
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
 });
 
 app.use('/api/user', userRouter);
